f469f93b65d1df9a5f4169ab2ecc021fe7206076
nikolinareic03 <nikolina.reic@gmail.com>
2020-10-22 13:21:36 +0200

    adding Navigation

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,16 @@ import React from "react"
 import ContactBar from '../components/contactBar'
 import NavigationHeader from '../components/NavigationHeader'
 import Logo from '../components/Logo'
+import NavigationBar from '../components/NavigationBar'
+import Combobox from '../components/Combobox'
 
 const IndexPage = () => (
   <main>
     <ContactBar />
     <NavigationHeader />
     <Logo />
+    <NavigationBar />
+    <Combobox />
   </main>
 )
 
