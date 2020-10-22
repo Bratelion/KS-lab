e69afb070b17ac8e68ed4d223a3f5eb55be58513
nikolinareic03 <nikolina.reic@gmail.com>
2020-10-22 13:14:33 +0200

    Adding Logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import ContactBar from '../components/contactBar'
 import NavigationHeader from '../components/NavigationHeader'
+import Logo from '../components/Logo'
 
 const IndexPage = () => (
   <main>
