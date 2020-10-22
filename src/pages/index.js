4508407d0068694b56b704e1e6133591801f1e9d
nikolinareic03 <nikolina.reic@gmail.com>
2020-10-22 13:48:44 +0200

    refactor to use props for .active

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,12 @@ import React from "react"
 
 import ContactBar from '../components/contactBar'
 import NavigationHeader from '../components/NavigationHeader'
-import Logo from '../components/Logo'
-import NavigationBar from '../components/NavigationBar'
-import Combobox from '../components/Combobox'
+
 
 const IndexPage = () => (
   <main>
     <ContactBar />
-    <NavigationHeader />
+    <NavigationHeader activeTab = "Home" />
   </main>
 )
 
