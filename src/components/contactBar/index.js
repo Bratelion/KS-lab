de35f6ebf3734c3f8e038f312bba7afd7f179602
Ante PICS <>
2020-11-05 13:35:47 +0100

    Rework: adding FontAwsome

--- src/components/contactBar/index.js
+++ src/components/contactBar/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import styles from './style.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
 
 const ContactBar = () => (
   <main className={styles.container}>
-    <span>example@email.com</span>
-    <span>+0385911122334</span>
+     <span>example@email.com</span>
+     <FontAwesomeIcon icon={faEnvelope} color="white" />
+     <span>+0385911122334</span>
+     <FontAwesomeIcon icon={faPhone} color="white"/>
   </main>
 )
 
